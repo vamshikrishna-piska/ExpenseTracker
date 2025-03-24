9bf4360084d9668ce76ed2c934c3a1bf7c1add1f
vamshikrishna-piska <vamshi.piska@gmail.com>
2025-03-24 09:11:40 +0530

    1. Added JWT authentication.Configured in settings.py 2. Added access token and refresh token endpoints in url.py. 3. Modified ExpenseViewSet to filter expenses by the logged-in user 4. Updated API authentication to use JWT tokens.

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +123,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
--- tracker/views.py
+++ tracker/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Expense
 from .serializers import ExpenseSerializer
 
 class ExpenseViewSet(viewsets.ModelViewSet):
-    queryset=Expense.objects.all()
-    serializer_class=ExpenseSerializer
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
+
